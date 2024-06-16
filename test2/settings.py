b7e34df76a7337e2932bb5e3ded15c1d7eb41611
kwesimain <opokulive@gmail.com>
2024-06-16 03:21:26 +0000

    updated files to fix server run error

--- test2/settings.py
+++ test2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3x$+f2lu3rktr6$=%f+wc!*5&kp=n*d(1l9ccfh2_y&loz&q%c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "0.0.0.0:8000"]
 
 
 # Application definition
